a79f7e8cee4332277af9eebe3e9b2e31611ec717
Maksym Zhukov <63158421+Maxi12323@users.noreply.github.com>
2020-06-16 17:36:35 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,68 @@
+
+// Game
+const cards = document.querySelectorAll('.memory-card');
+
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+   this.classList.add('flip');
+
+   if (!hasFlippedCard) {
+       hasFlippedCard = true;
+       firstCard = this;
+
+       return;
+   }
+       hasFlippedCard = false;
+       secondCard = this;
+
+       checkForMatching();
+}
+
+function checkForMatching() {
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+    isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    resetBoard();
+}
+
+function unflipCards() {
+    lockBoard = true;
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        resetBoard();
+    }, 1300);
+}
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+    });
+})();
+cards.forEach(card => card.addEventListener('click', flipCard));
+
+// POPUP
+
+let popup = document.getElementById('popup');
+let start = document.getElementById('start');
+
+function hidePopup() {
+    popup.style.opacity = "0"
+    popup.style.visibility = "hidden"
+}
+
+start.addEventListener('click', hidePopup);
